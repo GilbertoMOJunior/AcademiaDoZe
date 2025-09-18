a6b01d0bdc3e5e5c46ef2b897e2148c0dc71d9b0
Gilberto <Gilberto@DESKTOP-GMO>
2025-09-17 22:51:00 -0300

    template inicial maui

---
+++ AcademiaDoZe.Presentation/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AcademiaDoZe.Presentation
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AcademiaDoZe.Presentation/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AcademiaDoZe.Presentation
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
