58ece9dc8c281604ae59f027b616b4d7e7521651
Gilberto <Gilberto@DESKTOP-GMO>
2025-10-02 18:30:59 -0300

    atividade 10

--- AcademiaDoZe.Presentation/App.xaml.cs
+++ AcademiaDoZe.Presentation/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using AcademiaDoZe.Presentation.AppMaui;
-using Microsoft.Maui;
-using Microsoft.Maui.Controls;
+using AcademiaDoZe.Presentation.AppMaui.Message;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace AcademiaDoZe.Presentation
 {
@@ -9,11 +9,35 @@ namespace AcademiaDoZe.Presentation
         public App()
         {
             InitializeComponent();
+
+            // aplicar o tema salvo nas preferências
+            AplicarTema();
+
+            // assinar para receber mensagens de alteração de preferências
+            // toda vez que o usuário alterar o tema, essa mensagem será enviada
+            // e o tema será atualizado
+            WeakReferenceMessenger.Default.Register<TemaPreferencesUpdatedMessage>(this, (r, m) =>
+
+            {
+                // m.Value contém o valor enviado na mensagem
+                AplicarTema();
+            });
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+        }
+
+        private void AplicarTema()
+        {
+            UserAppTheme = Preferences.Get("Tema", "system") switch
+            {
+                "light" => AppTheme.Light,
+                "dark" => AppTheme.Dark,
+                _ => AppTheme.Unspecified,
+
+            };
         }
     }
 }
